ad7223fb7400ce7525242e7dec0e6d3966d4899e
Or Partush <orptzz@gmail.com>
2020-05-07 16:46:58 +0300

    contact fix

--- src/cmps/contact.vue
+++ src/cmps/contact.vue
@@ -3,7 +3,7 @@
     <p class="intro">03. Let's Talk</p>
     <p class="title">Contact Me</p>
     <p class="txt">
-      Currently I'm looking for Full-Stack / Frontend Developer position.
+      Currently I'm looking for a Full-Stack / Frontend Developer position.
       <br />If you've liked what you just saw or if you just want to say hi, feel free to contact me!
     </p>
     <a href="mailto:orpt.dev@gmail.com" target="_blank">Say Hello</a>
